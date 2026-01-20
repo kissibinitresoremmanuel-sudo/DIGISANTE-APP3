a8d56e9760ad2a70b1e51f1771455b3b3823e4b7
kissi <kissibinitresoremmanuel@gmail.com>
2026-01-20 11:21:57 +0000

    Update prediction_model.dart

--- lib/models/prediction_model.dart
+++ lib/models/prediction_model.dart
@@ -1,4 +1,3 @@
-
 enum RiskLevel {
   low,
   medium,
@@ -10,9 +9,39 @@ class Prediction {
   final String description;
   final DateTime predictionDate;
 
+  // --- AJOUT DES PARAMÈTRES POUR TON IA ---
+  final double? temperature;
+  final int? spo2;
+  final int? bpm;
+  final int? age;
+  // Tu peux ajouter les autres ici (sexe, toux, etc.)
+
   Prediction({
     required this.riskLevel,
     required this.description,
     required this.predictionDate,
+    this.temperature,
+    this.spo2,
+    this.bpm,
+    this.age,
   });
+
+  // --- CETTE FONCTION TRANSFORME LES DONNÉES POUR TON PYTHON ---
+  Map<String, dynamic> toMapPourIA() {
+    return {
+      "age": age ?? 30, // Valeur par défaut si vide
+      "sexe": 1, 
+      "fumeur": 0,
+      "annees_tabagisme": 0,
+      "temperature_corporelle": temperature ?? 37.0,
+      "toux": 1,
+      "essoufflement": 1,
+      "fatigue": 1,
+      "douleur_thoracique": 1,
+      "frequence_cardiaque": bpm ?? 80,
+      "spo2": spo2 ?? 98,
+      "temperature_ambiante": 25,
+      "humidite": 50,
+    };
+  }
 }
